bf0dd31a86b049b98e7b16dca599b389408e8dbe
jevniky <p.javnicky@gmail.com>
2015-11-13 20:08:51 +0100

    thermostat 1.0
    All working

--- config.h
+++ config.h
@@ -1,44 +1,53 @@
 /*KY-40 INIT*/
-#define MAX_POS 60
-#define MIN_POS 30
+#define MAX_POS 60 // max temp 30
+#define MIN_POS 30 // min temp 15
+
 enum PinAssignments {
-  pinA = 2,   // DT pin
-  pinB = 3,   // CLK pin
-  pinSW = 4    // SW pin
+    pinA = 2, // DT pin
+    pinB = 3, // CLK pin
+    pinSW = 4 // SW pin
 };
-volatile  int encoderPos = MIN_POS;  // a counter for the dial
-int lastReportedPos = MIN_POS-1;   // change management
-static boolean rotating = false;      // debounce management
-// interrupt service routine vars
-boolean A_set = false;              
+volatile int encoderPos = MIN_POS; // a counter for the dial
+int lastReportedPos = MIN_POS-1; // change management
+static boolean rotating = false; // debounce management
+// interrupt service routine variables
+boolean A_set = false; 
 boolean B_set = false;
 
 /*TEMP setup*/
-float setTemp = MIN_POS*0.5;
-// Data wire is plugged into port 2 on the Arduino
+float setTemp = MIN_POS*0.5; // Starting point for temperature setting.
+float temperature = 0.0; // measured temperature will be stored here
+
+// Data wire is plugged into port 5 on the Arduino
 #define ONE_WIRE_BUS 5
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+
+// Setup a oneWire instance to communicate with my ds18b20 sensor
 OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
+
+// Pass the oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
-// arrays to hold device address
+
+// array to hold d18b20 address
 DeviceAddress thermometer = { 0x28, 0xFF, 0xE3, 0xD4, 0x67, 0x14, 0x03, 0x39 };
+// make sure you will find out the address of your sensor
+
+// pins to show if heating or cooling is needed (relays)
+int heatPin = 8;
+int coolPin = 9;
 
 /*DELAYS*/
-unsigned long previousMillis = 0;  
+unsigned long previousMillis = 0; 
 const long interval = 3000;
 
 /*Ethernet*/
-byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-EthernetClient ethClient;
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 }; // I've made this address up
+EthernetClient ethClient; // start an instance
 
 /* MQTT */
 PubSubClient mqttClient(ethClient);
 
 char tempBuff[10];
 char setTempBuff[10];
-float temperature = 0.0;
 
-/*RELAYS*/
-int heatPin = 8;
-int coolPin = 9;
+char payload1;
+
--- thermostat.ino
+++ thermostat.ino
@@ -6,160 +6,165 @@
 
 
 void setup() {
-  pinMode(pinA, INPUT_PULLUP); // enabling pullups
-  pinMode(pinB, INPUT_PULLUP);
-  pinMode(pinSW, INPUT_PULLUP);
+    //KY-40:
+    pinMode(pinA, INPUT_PULLUP); // enabling pullups
+    pinMode(pinB, INPUT_PULLUP);
+    pinMode(pinSW, INPUT_PULLUP);
 
-  //set up output pins
-  pinMode(heatPin,OUTPUT);
-  pinMode(coolPin,OUTPUT);
+    //set up output pins
+    pinMode(heatPin,OUTPUT); // relays are connected here
+    pinMode(coolPin,OUTPUT);
 
-  // encoder pin on interrupt 0 (pin 2)
-  attachInterrupt(0, doEncoderA, CHANGE);
-  // encoder pin on interrupt 1 (pin 3)
-  attachInterrupt(1, doEncoderB, CHANGE);
+    // encoder pin on interrupt 0 (pin 2)
+    attachInterrupt(0, doEncoderA, CHANGE);
+    // encoder pin on interrupt 1 (pin 3)
+    attachInterrupt(1, doEncoderB, CHANGE);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  sensors.begin();
-  sensors.setResolution(thermometer, 9);
+    sensors.begin();
+    // set the resolution to 9 bit. You can set it up to 12 if you want really precise values
+    sensors.setResolution(thermometer, 9);
 
-  mqttClient.setServer("mqtt.polyrem.sk", 1883);
-  mqttClient.setCallback(callback);
+    connectToInternet();
+    delay(1500); // allow the hardware to sort itself out
 
-  connectToEthernet();
-  delay(1500);
+    mqttClient.setServer("mqtt.polyrem.sk", 1883); // set server address and port
+mqttClient.setCallback(callback); // set callback function (function that handles incoming messages
 }
 
-// main loop, work is done by interrupt service routines, this one only prints stuff
 void loop() {
-  if (!mqttClient.connected()) {
-    reconnect();
-  }
-
-  rotating = true;  // reset the debouncer
-  //to protect from going over min & max values
-  if (encoderPos <= MIN_POS - 1 ) {
-    encoderPos = MIN_POS;
-    lastReportedPos = MIN_POS + 1;
-  } else if (encoderPos >= MAX_POS + 1) {
-    encoderPos = MAX_POS;
-    lastReportedPos = MAX_POS - 1;
-  } else {
-    if (lastReportedPos != encoderPos) { // Serial print
-      setTemp = encoderPos * 0.5;
-      Serial.print("Set: ");
-      Serial.println(setTemp);
-      dtostrf(setTemp, 3, 2, setTempBuff);
-      mqttClient.publish("temperature", setTempBuff);
-      mqttClient.publish("setTemp", setTempBuff);
-      lastReportedPos = encoderPos;
+    if (!mqttClient.connected()) { // if the client is not connected - reconnect
+        reconnect();
     }
-  }
-  if (digitalRead(pinSW) == LOW )  { // push to restart to 0
-    encoderPos = 0;
-    lastReportedPos = 1;
-    delay (10);
-  }
+  
+    rotating = true; // reset the debouncer
+    //to protect from going over min & max values
+    if (encoderPos <= MIN_POS - 1 ) {
+        encoderPos = MIN_POS;
+        lastReportedPos = MIN_POS + 1;
+    } else if (encoderPos >= MAX_POS + 1) {
+        encoderPos = MAX_POS;
+        lastReportedPos = MAX_POS - 1;
+    } else {
+        // is between min and max values
+        if (lastReportedPos != encoderPos) {
+            // divide encoder position value by 2 to get 0.5 degree precision
+            // store this value as set temperature
+            setTemp = encoderPos * 0.5;
+            // and print it to serial monitor
+            Serial.print("Set: ");
+            Serial.println(setTemp);
+            dtostrf(setTemp, 3, 2, setTempBuff);
+            mqttClient.publish("setTemp", setTempBuff);
+            lastReportedPos = encoderPos;
+        }
+    }
+    // in case you press the integrated button on a KY-40
+    if (digitalRead(pinSW) == LOW ) {
+        // restart to minimal position
+        encoderPos = MIN_POS;
+        lastReportedPos = MIN_POS+1;
+        delay (10);
+        // you can restar to default 20 degrees for example, or do whatever you like
+    }
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    //request temperature from sensors
-    sensors.requestTemperatures();
-    //print temperature out with my function
-    //printTemperature(thermometer);
-    temperature = sensors.getTempC(thermometer);
-    Serial.print("Got: ");
-    //Serial.println(temperature);
-    dtostrf(temperature, 3, 2, tempBuff);
-    Serial.println(tempBuff);
-    mqttClient.publish("temperature", tempBuff);
-    editTemperature(setTemp,temperature,1);
-  }
-  mqttClient.loop();
+    // here I measure and print the temperature out without delay() using millis()
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        // request temperature from sensor
+        sensors.requestTemperatures();
+        // store the sensor value
+        temperature = sensors.getTempC(thermometer);
+        // and print it out
+        Serial.print("Got: ");
+        Serial.println(temperature);
+        dtostrf(temperature, 3, 2, tempBuff); // create a string from float value
+        mqttClient.publish("temperature", tempBuff); // client.publish(topic,message)
+        // my function to edit temperature:
+        editTemperature(setTemp,temperature,1);
+        // the last parameter is precision of editing
+    }
+    mqttClient.loop();
 }
-
 // Interrupt on A changing state
 void doEncoderA() {
-  if ( rotating ) delay (1);  // wait a little until the bouncing is done
-  // Test transition, did things really change?
-  if ( digitalRead(pinA) != A_set ) { // debounce once more
-    A_set = !A_set;
-    // adjust counter + if A leads B
-    if ( A_set && !B_set ) encoderPos += 1;
-    rotating = false;  // no more debouncing until loop() hits again
-  }
+    if ( rotating ) delay (1); // wait a little until the bouncing is done
+    // Test transition, did things really change?
+    if ( digitalRead(pinA) != A_set ) { // debounce once more
+        A_set = !A_set;
+        // adjust counter + if A leads B
+        if ( A_set && !B_set ) encoderPos += 1;
+        rotating = false; // no more debouncing until loop() hits again
+    }
 }
 // Interrupt on B changing state, same as A above
 void doEncoderB() {
-  if ( rotating ) delay (1);
-  if ( digitalRead(pinB) != B_set ) {
-    B_set = !B_set;
-    //  adjust counter - 1 if B leads A
-    if ( B_set && !A_set ) encoderPos -= 1;
-    rotating = false;
-  }
-}
-
-void connectToEthernet() {
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for (;;)
-      ;
-  }
-  // print your local IP address:
-  Serial.print("My IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print(".");
-  }
-  Serial.println();
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-void reconnect() {
-  // Loop until we're reconnected
-  while (!mqttClient.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (mqttClient.connect("arduinoClient")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      //mqttClient.publish("outTopic", "hello world");
-      // ... and resubscribe
-      mqttClient.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" try again in 3 seconds");
-      // Wait 3 seconds before retrying
-      delay(3000);
+    if ( rotating ) delay (1);
+    if ( digitalRead(pinB) != B_set ) {
+        B_set = !B_set;
+        // adjust counter - 1 if B leads A
+        if ( B_set && !A_set ) encoderPos -= 1;
+        rotating = false;
     }
-  }
 }
 void editTemperature(float set, float actual, int limit) {
-  if (actual > set+limit) {
-    Serial.println("Need to cool...");
-    digitalWrite(heatPin,LOW);
-    digitalWrite(coolPin,HIGH);
-  } else if (actual < set-limit) { 
-    Serial.println("Need to heat...");
-    digitalWrite(coolPin,LOW);
-    digitalWrite(heatPin,HIGH);
-  } else {
-    Serial.println("Temperature is OK");
-    digitalWrite(heatPin,LOW);
-    digitalWrite(coolPin,LOW);
-  }
+    if (actual > set+limit) {
+        Serial.println("Need to cool...");
+        digitalWrite(heatPin,LOW);
+        digitalWrite(coolPin,HIGH);
+    } else if (actual < set-limit) { 
+        Serial.println("Need to heat...");
+        digitalWrite(coolPin,LOW);
+        digitalWrite(heatPin,HIGH);
+    } else {
+        Serial.println("Temperature is OK");
+        digitalWrite(heatPin,LOW);
+        digitalWrite(coolPin,LOW);
+    }
+}
+void connectToInternet() {
+    if (Ethernet.begin(mac) == 0) { // if there is error
+        Serial.println("Failed to configure Ethernet using DHCP");
+        // no point in carrying on, so do nothing forevermore:
+        for (;;)
+        ;
+    }
+    // otherwise - print your local IP address:
+    Serial.print("My IP address: ");
+    for (byte thisByte = 0; thisByte < 4; thisByte++) {
+        // print the value of each byte of the IP address:
+        Serial.print(Ethernet.localIP()[thisByte], DEC);
+        Serial.print(".");
+    }
+    Serial.println();
+}
+void reconnect() {
+    // Loop until we're reconnected
+    while (!mqttClient.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (mqttClient.connect("arduinoClient")) {
+            Serial.println("connected");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" try again in 3 seconds");
+            // Wait 3 seconds before retrying
+            delay(3000);
+        }
+    }
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+    for (int i = 0; i < length; i++) {
+        if (i==0) setTemp = setTemp + 10*((int)payload[i]-48);
+        if (i==1) setTemp = setTemp + ((int)payload[i]-48);
+        if (i==3) setTemp = setTemp + 0.1*((int)payload[i]-48);
+        if (i==4) setTemp = setTemp + 0.01*((int)payload[i]-48);
+    }
+    Serial.print("Set temp from callback: ");
+    Serial.println(setTemp);
+    encoderPos = setTemp*2;
+    lastReportedPos = encoderPos;
 }
